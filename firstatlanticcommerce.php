3fd66c1e83d690eafd7cd3107663f99f77f57100
Michael Dekker <git@michaeldekker.nl>
2017-05-24 20:44:19 +0200

    Bump to 0.8.0

--- firstatlanticcommerce.php
+++ firstatlanticcommerce.php
@@ -71,7 +71,7 @@ class FirstAtlanticCommerce extends PaymentModule
     {
         $this->name = 'firstatlanticcommerce';
         $this->tab = 'payments_gateways';
-        $this->version = '0.7.2';
+        $this->version = '0.8.0';
         $this->author = 'thirty bees';
         $this->need_instance = 1;
 
